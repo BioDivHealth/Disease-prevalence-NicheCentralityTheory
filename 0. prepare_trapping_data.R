3880ad5d5997163a934f2f3b437a0b36efe98383
Harry Gordon <116832901+HGord2022@users.noreply.github.com>
2025-01-07 15:18:55 +0000

    Harry continued data manipulation and visualisation

--- 0. prepare_trapping_data.R
+++ 0. prepare_trapping_data.R
@@ -4,15 +4,32 @@ library(ggplot2)
 
 df <- readRDS("./Data/trapping_data_NC.rds")
 
-unique_sites_df <- df %>%
-  count(host_name, sort = TRUE) %>%        # Count occurrences of each host_name and sort by frequency
-  slice(1:20) %>%                          # Keep only the top 20 most frequent host_name values
-  select(host_name) %>%                    # Select only the host_name column
-  inner_join(df, by = "host_name") %>%     # Join back with the original dataframe to filter rows
-  distinct(host_name, start_date, locality) %>%  # Keep unique combinations of host_name, start_date, locality
-  arrange(host_name) %>%                   # Sort by host_name in ascending order
-  left_join(df %>%                         # Join with coordinate data
-              distinct(locality, decimalLatitude, decimalLongitude), # Keep first pair for each locality
-            by = "locality")
+df <- df %>%
+  filter(!is.na(decimalLatitude) & !is.na(decimalLongitude))
 
-saveRDS(unique_sites_df, file = "./Data/clean_sdm_data.rds")
+# Summarize the 'count' for each unique combination of coordinates
+summed <- df %>%
+  group_by(locality, decimalLatitude, decimalLongitude) %>%
+  summarise(total_tested = sum(as.numeric(tested)),
+            total_positive = sum(as.numeric(positive)), 
+            .groups = "drop")
+
+# Get unique combinations and add the corresponding coordinate pairs and summed 'count' for each locality
+unique_sites_df <- df %>%
+  distinct(host_name, locality) %>%  # Keep unique combinations of host_name, start_date, locality
+  left_join(summed, by = c("locality"))   # Join with summarized 'count' data
+
+### Keep only the top 20 species
+
+# Filter to keep only the 20 most common host_name values
+top_20_host_names <- unique_df %>%
+  count(host_name, sort = TRUE) %>%       # Count occurrences of each host_name and sort by frequency
+  slice(1:20) %>%                         # Keep only the top 20 most frequent host_name values
+  select(host_name)                       # Select only the host_name column
+
+# Filter the original dataframe to include only rows with the top 20 host_names
+unique_sites_top_20 <- unique_sites_df %>%
+  semi_join(top_20_host_names, by = "host_name")  # Keep only rows with host_name in top 20
+
+
+saveRDS(unique_sites_top_20, file = "./Data/clean_site_data.rds")
--- 1. visualise_trapping_data.R
+++ 1. visualise_trapping_data.R
@@ -2,22 +2,64 @@ library(dplyr)
 library(ggplot2)
 library(maps)
 
-df <- readRDS("./Data/clean_sdm_data.rds")
+top_20_df <- readRDS("./Data/clean_site_data.rds")
 
+host_site_summary <- top_20_df %>%
+  group_by(host_name) %>%
+  summarise(unique_coordinates = n_distinct(decimalLatitude, decimalLongitude))
 
+#########
+# Barplot of number of locations per species
 
-
-
-
-
-
-
-
-
+ggplot(host_site_summary, aes(x = reorder(host_name, unique_coordinates), y = unique_coordinates)) +
+  geom_bar(stat = "identity", fill = "steelblue", alpha = 0.7) +
+  coord_flip() +
+  labs(
+    title = "Species list by unique coordinates and prevalence",
+    x = "Species",
+    y = "Number of Unique Coordinates"
+  ) +
+  theme_minimal()
 
 #########
 # Maps of trapped rodents
 ###
+
+# Filter the data for a specific host_name
+host_name_group <- "Apodemus flavicolus"  # Change to your desired host_name
+filtered_df <- top_20_df %>%
+  filter(host_name == host_name_group)
+
+# Plot the coordinates on a map of Volyn Oblast, Ukraine
+ggplot(data = filtered_df, aes(x = decimalLongitude, y = decimalLatitude)) +
+  borders("world", region = "Ukraine", colour = "gray85", fill = "gray80") +  # Add map borders for Ukraine
+  geom_point(color = "red", size = 3) +  # Plot the points
+  labs(title = paste("Locations for Host:", host_name_group, "in Volyn Oblast, Ukraine"),
+       x = "Longitude",
+       y = "Latitude") +
+  coord_fixed(xlim = c(23.96, 24.13), ylim = c(51.175, 51.3)) +  # Zoom into Volyn Oblast's coordinates
+  theme_minimal()
+
+
+
+# Filter the data for a specific host_name
+host_name_group <- "Suncus murinus"  # Change to your desired host_name
+filtered_df <- top_20_df %>%
+  filter(host_name == host_name_group)
+
+# Plot the coordinates on a map of Asia
+ggplot(data = filtered_df, aes(x = decimalLongitude, y = decimalLatitude)) +
+  borders("world", colour = "gray85", fill = "gray80") +  # Add map borders for Asia
+  geom_point(color = "red", size = 3) +  # Plot the points
+  labs(title = paste("Locations for Host:", host_name_group, "in Asia"),
+       x = "Longitude",
+       y = "Latitude") +
+  coord_fixed(xlim = c(60, 135), ylim = c(-10, 38)) +  # Adjust limits for Asia
+  theme_minimal()
+
+
+
+
 
 # plot mus musculus sites in uruguay
 host_name_group <- "Mus musculus"
@@ -32,6 +74,11 @@ ggplot(data = filtered_df, aes(x = decimalLongitude, y = decimalLatitude)) +
        y = "Latitude") +
   coord_fixed(xlim = c(-59, -53), ylim = c(-35, -30)) +  # Zoom into Uruguay's coordinates
   theme_minimal()
+
+
+
+
+
 
 # plot mus musculus sites in uruguay
 host_name_group <- "Baiomys taylori"
